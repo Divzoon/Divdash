79fed7cd895fc58122d7ecb589c08dacf5902a5b
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-10-02 02:25:26 +0200

    bug2

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-import { unstable_getServerSession } from "next-auth/next"
-import { authOptions } from "./api/auth/[...nextauth]"
 const PUBLIC_FILE = /\.(.*)$/;
 
 export function middleware(request: NextRequest) {
@@ -12,18 +10,4 @@ export function middleware(request: NextRequest) {
   return shouldHandleLocale
     ? NextResponse.redirect(`/en${request.nextUrl.href}`)
     : undefined;
-}
-export default   async (req, res) => {
-  const session = await unstable_getServerSession(req, res, authOptions)
-
-  if (session) {
-    res.send({
-      content:
-        "This is protected content. You can access this content because you are signed in.",
-    })
-  } else {
-    res.send({
-      error: "You must be signed in to view the protected content on this page.",
-    })
-  }
 }
