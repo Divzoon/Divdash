8615a58ff1475edbd0788d7105de584a7da4ed5f
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-10-02 02:19:46 +0200

    bug

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import { unstable_getServerSession } from "next-auth/next"
+import { authOptions } from "./api/auth/[...nextauth]"
 const PUBLIC_FILE = /\.(.*)$/;
 
 export function middleware(request: NextRequest) {
@@ -11,4 +12,18 @@ export function middleware(request: NextRequest) {
   return shouldHandleLocale
     ? NextResponse.redirect(`/en${request.nextUrl.href}`)
     : undefined;
+}
+export default   async (req, res) => {
+  const session = await unstable_getServerSession(req, res, authOptions)
+
+  if (session) {
+    res.send({
+      content:
+        "This is protected content. You can access this content because you are signed in.",
+    })
+  } else {
+    res.send({
+      error: "You must be signed in to view the protected content on this page.",
+    })
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,11 @@
 import React from "react";
-import en from "../translation/en";
-import ar from "../translation/ar";
-import { useRouter } from "next/router";
+import DashHeader from "../components/DashHeader";
+
 function index() {
-  const router = useRouter();
-  const { locale } = router;
-  const trans = locale === "en" ? en : ar;
- let ltr =locale === "en" ?false:true;
+
   return (
-  <div className={`${"dark:bg-[#0b0e11] dark:text-gray-200 bg-gray-300   h-full lg:pl-[310px]  pt-[60px]  "}${locale === "ar"?"lg:pr-[50px] pr-[10px]":null}`}>
-    <div dir={locale === "en"?"ltr":"rtl"}  >
-      <h1  style={{ fontFamily: "Poppins-bold", width: "400" }} className="text-4xl  lg:text-6xl pb-0 p-2">{trans.Dashboard}</h1>
-    <p className=" font-semibold	dark:text-gray-400 text-gray-900   opacity-80 pt-0 p-2 text-xl backdrop-blur-lg	">{trans.DashPcalltoaction}</p>
-    </div>
+  <div className="dark:bg-[#0b0e11] dark:text-gray-200 bg-gray-300   h-full lg:pl-[310px]  pt-[60px]  ">
+<DashHeader/>
   </div>
   );
 }
